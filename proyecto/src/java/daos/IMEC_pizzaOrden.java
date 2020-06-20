8b7e457b66f3682ef0b763f6624728c54411cfb8
diego <diego@DESKTOP-8EAKPMP>
2020-06-19 20:46:37 -0600

    Model y Daos 1

--- proyecto/src/java/daos/IMEC_pizzaOrden.java
+++ proyecto/src/java/daos/IMEC_pizzaOrden.java
@@ -2,10 +2,10 @@ package daos;
 
 public enum IMEC_pizzaOrden {
     INSERTAR("INSERT INTO relacion_pizza_orden (orden,pizza,cantidad) VALUES (?, ?, ?); "),
-    MODIFICAR("UPDATE relacion_pizza_orden SET apellidos=?, nombre=? WHERE id=?; "),
+    //MODIFICAR("UPDATE relacion_pizza_orden SET apellidos=?, nombre=? WHERE id=?; "),
     EXCLUIR("DELETE FROM estudiante WHERE id=?; "),
-    CONSULTAR("SELECT id, apellidos, nombre FROM estudiante WHERE id=?; "),
-    LISTAR("SELECT id, apellidos, nombre FROM estudiante ORDER BY apellidos; ");
+    //CONSULTAR("SELECT id, apellidos, nombre FROM estudiante WHERE id=?; "),
+    LISTAR("SELECT orden FROM relacion_pizza_orden WHERE orden = ? ; ");
 
     IMEC_pizzaOrden(String comando) {
         this.comando = comando;
