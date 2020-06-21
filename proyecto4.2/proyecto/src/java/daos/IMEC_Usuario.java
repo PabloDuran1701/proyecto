1641ea5ac7746a24899c69ca029028b2fb6c57d8
diego <diego@DESKTOP-8EAKPMP>
2020-06-20 21:20:29 -0600

    Usuario

--- proyecto4.2/proyecto/src/java/daos/IMEC_Usuario.java
+++ proyecto4.2/proyecto/src/java/daos/IMEC_Usuario.java
@@ -1,7 +1,8 @@
 package daos;
 
 public enum IMEC_Usuario {
-    INSERTAR("INSERT INTO usuario (id, tipo, password, nombre, apellido1, direccion, telefono) VALUES (?,?,?,?,?,?,?); "),
+    INSERTAR("INSERT INTO usuario (id, tipo, password, nombre, "
+            + "apellido1, direccion, telefono) VALUES (?,?,?,?,?,?,?); "),
     MODIFICAR("UPDATE usuario SET apellidos=?, nombre=? WHERE id=?; "),
     EXCLUIR("DELETE FROM usuario WHERE id=?; "),
     CONSULTAR("SELECT id, tipo, password, nombre, apellido1, direccion, telefono FROM usuario WHERE id=?; "),
